bb87b9ab6843a83e36e439caaa83f630ee9bee0b
IconicLuck <uhasib959@gmail.com>
2023-08-06 00:08:03 +0600

    added navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,11 +12,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <div>
-        <Footer></Footer>
-      </div>
+    <html lang="en" data-theme="dark" className='transition-all'>
+
+      <body className={inter.className}>
+        <div>
+          <Navbar></Navbar>
+        </div>
+        {children}
+        <div>
+          <Footer></Footer>
+        </div>
+      </body>
+
     </html>
   )
 }
