b5bfc94ccbfc7ffef50b9d5e02c37e49d176c0a7
IconicLuck <uhasib959@gmail.com>
2023-08-06 00:19:29 +0600

    created a navlink to component to use it later

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+const NavLink = ({children, href, exact = false, activeClassName, ...props}) => {
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NavLink;
