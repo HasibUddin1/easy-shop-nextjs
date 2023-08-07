ef5ff85e847a93e3f5d9b127ec6cd8094db27c14
IconicLuck <uhasib959@gmail.com>
2023-08-07 19:21:34 +0600

    added function for google login, log out

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,29 +1,65 @@
 import AuthContext from "@/contexts/AuthContext";
-import auth from "@/firebase/firebase.auth";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import auth, { googleProvider } from "@/firebase/firebase.auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { useEffect, useState } from "react";
 
 
-const AuthProviders = () => {
+const AuthProviders = ({children}) => {
 
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
 
     const createUser = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const signIn = (email, password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
-    const authInfo = {
+    const updateUserInfo = (user, name, photo) => {
+        setLoading(true)
+        return updateProfile(user, {
+            displayName: name, photoURL: photo
+        })
+    }
 
+    const googleLogin = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            console.log(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        updateUserInfo,
+        googleLogin,
+        logOut
     }
 
     return (
         <AuthContext.Provider value={authInfo}>
-            
+            {children}
         </AuthContext.Provider>
     );
 };
