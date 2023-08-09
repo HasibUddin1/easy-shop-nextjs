f3195b46911358e55d2eb9dec6796240fe95da5a
IconicLuck <uhasib959@gmail.com>
2023-08-09 20:30:40 +0600

    displayed all products on products page

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,3 +1,4 @@
+import Categories from "./Categories";
 import HeroSlider from "./HeroSlider";
 
 
@@ -6,6 +7,9 @@ export default function Home() {
   return (
     <div>
       <HeroSlider></HeroSlider>
+      <div className="w-9/12 mx-auto">
+        <Categories></Categories>
+      </div>
     </div>
   )
 }
